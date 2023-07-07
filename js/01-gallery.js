34564cd5906bf5a09429725370023de75247ba38
DmShko <vombat_schev@i.ua>
2023-07-07 21:02:31 +0300

    goit-js-hw-07-3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,8 +4,28 @@ import { galleryItems } from './gallery-items.js';
 // add event to father element
 const galleryElement = document.querySelector("ul.gallery");
 
+function addMarkup() {
+// get atribut from 'galleryItems'
+    return galleryItems.map(element => 
+      `
+     <li class="gallery__item">
+        <a class="gallery__link" href="${element.original}">
+            <img 
+                class="gallery__image"
+                src="${element.preview}"  
+                data-source="${element.original}"
+                alt="${element.description}" 
+            > 
+        </a>
+      </li>`
+    ).join("");
+}
 
+// call create murkup function
+const getAttribute = addMarkup();
 
+// add content to 'gallery'
+galleryElement.innerHTML = getAttribute;
 // event handler
 galleryElement.addEventListener('click', e => {
     
